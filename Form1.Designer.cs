c3d3d7e764872655a770eb05ebe650193d2464b6
Ice <your name>
2020-02-27 12:06:47 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace To_Do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddToDo = new System.Windows.Forms.Button();
+            this.txtNewToDo = new System.Windows.Forms.TextBox();
+            this.clsToDo = new System.Windows.Forms.CheckedListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblDeleteItem = new System.Windows.Forms.Label();
+            this.LstDone = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What do you need to do?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Things todo ";
+            // 
+            // btnAddToDo
+            // 
+            this.btnAddToDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddToDo.Location = new System.Drawing.Point(464, 29);
+            this.btnAddToDo.Name = "btnAddToDo";
+            this.btnAddToDo.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToDo.TabIndex = 2;
+            this.btnAddToDo.Text = "Add";
+            this.btnAddToDo.UseVisualStyleBackColor = true;
+            this.btnAddToDo.UseWaitCursor = true;
+            this.btnAddToDo.Click += new System.EventHandler(this.btnAddToDo_Click);
+            // 
+            // txtNewToDo
+            // 
+            this.txtNewToDo.Location = new System.Drawing.Point(39, 29);
+            this.txtNewToDo.Name = "txtNewToDo";
+            this.txtNewToDo.Size = new System.Drawing.Size(406, 20);
+            this.txtNewToDo.TabIndex = 3;
+            // 
+            // clsToDo
+            // 
+            this.clsToDo.FormattingEnabled = true;
+            this.clsToDo.Location = new System.Drawing.Point(39, 84);
+            this.clsToDo.Name = "clsToDo";
+            this.clsToDo.Size = new System.Drawing.Size(388, 94);
+            this.clsToDo.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(39, 194);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(188, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Remove checked items";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblDeleteItem
+            // 
+            this.lblDeleteItem.AutoSize = true;
+            this.lblDeleteItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeleteItem.Location = new System.Drawing.Point(36, 242);
+            this.lblDeleteItem.Name = "lblDeleteItem";
+            this.lblDeleteItem.Size = new System.Drawing.Size(94, 17);
+            this.lblDeleteItem.TabIndex = 6;
+            this.lblDeleteItem.Text = "Deleted items";
+            // 
+            // LstDone
+            // 
+            this.LstDone.FormattingEnabled = true;
+            this.LstDone.Location = new System.Drawing.Point(39, 292);
+            this.LstDone.Name = "LstDone";
+            this.LstDone.Size = new System.Drawing.Size(388, 95);
+            this.LstDone.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(39, 415);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.LstDone);
+            this.Controls.Add(this.lblDeleteItem);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.clsToDo);
+            this.Controls.Add(this.txtNewToDo);
+            this.Controls.Add(this.btnAddToDo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "To Do List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddToDo;
+        private System.Windows.Forms.TextBox txtNewToDo;
+        private System.Windows.Forms.CheckedListBox clsToDo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblDeleteItem;
+        private System.Windows.Forms.ListBox LstDone;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToDo_Click(object sender, EventArgs e)
+        {
+            //Remove any spaces 
+            string newItem = txtNewToDo.Text.Trim();
+
+            if(!string.IsNullOrWhiteSpace(newItem))
+            {
+                //use contains to check if item is already in items collection 
+                if (clsToDo.Items.Contains(newItem))
+                {
+                    MessageBox.Show("You already added that item", "Error");
+                }
+                else
+                {
+                    //USe add to add new item to end of items collection
+                    clsToDo.Items.Add(newItem);
+                    txtNewToDo.Text = "";
+                }
+            }
+            // no else , just ignore empty input 
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // make new list 
+            List<string> doneItems = new List<string>();
+
+            foreach (string item in clsToDo.CheckedItems)
+            {
+                doneItems.Add(item);
+            }
+            //remove from clsTodo.item
+            //add to lstdone.item
+            foreach(string item in doneItems)
+            {
+                clsToDo.Items.Remove(item); //remove by value 
+                LstDone.Items.Add(item);
+            }
+
+        }
+
+    }
+}
